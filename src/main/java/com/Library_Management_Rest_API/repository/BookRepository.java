4fc5ad7c68e5a1921716ea3cf2314fed5106d388
ErikV121 <ErikVasquez121@gmail.com>
2024-07-12 17:23:07 -0400

    Implemented custom query methods

--- src/main/java/com/Library_Management_Rest_API/repository/BookRepository.java
+++ src/main/java/com/Library_Management_Rest_API/repository/BookRepository.java
@@ -2,13 +2,13 @@ package com.Library_Management_Rest_API.repository;
 
 import com.Library_Management_Rest_API.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
 
+
 public interface BookRepository extends JpaRepository<Book, Long> {
-    //findByCategory
-    //findByAuthor
-    //findByTitleContaining
-    //findByIsAvailable
+    Optional<Book> findByIsbn(String isbn);
+    List<Book> findByAuthorIgnoreCase(String author);
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByAvailable (boolean isAvailable);
 }
