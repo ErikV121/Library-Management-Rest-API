5975fae24c151e6803cd428e401e7e90db27490d
ErikV121 <ErikVasquez121@gmail.com>
2024-07-12 17:19:46 -0400

    Added field constraints

--- src/main/java/com/Library_Management_Rest_API/payload/BookDto.java
+++ src/main/java/com/Library_Management_Rest_API/payload/BookDto.java
@@ -1,31 +1,30 @@
-package com.Library_Management_Rest_API.entity;
+package com.Library_Management_Rest_API.payload;
 
-import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-@Entity
-@Table(name = "Book")
-public class Book extends BaseEntity {
+//response & request DTO for books
+public class BookDto {
 
-    @Column(name = "title", nullable = false)
+    @Size(min = 1, message = "title should have atleast 1 character")
     private String title;
-    @Column(name = "author", nullable = false)
+    @NotBlank(message = "author can not be blank")
     private String author;
-    @Column(name = "isbn",nullable = false, unique = true)
-    private String isbn;
-    @Column(name = "publisher")
+    @NotBlank(message = "publisher can not be blank")
     private String publisher;
-    @Column(name = "available", nullable = false)
+    @Size(max = 13, message = "isbn can not be more than 13 characters ")
+    private String isbn;
     private boolean available;
 
-    public Book(){
+    public BookDto(){
 
     }
 
-    public Book(String title, String author, String isbn, String publisher,boolean available) {
+    public BookDto(String title, String author, String publisher, String isbn, boolean available) {
         this.title = title;
         this.author = author;
-        this.isbn = isbn;
         this.publisher = publisher;
+        this.isbn = isbn;
         this.available = available;
     }
 
@@ -45,20 +44,20 @@ public class Book extends BaseEntity {
         this.author = author;
     }
 
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
     public String getPublisher() {
         return publisher;
     }
 
     public void setPublisher(String publisher) {
         this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
     }
 
     public boolean isAvailable() {
