1e3e9262aa63be5427a7402cc81b75a8c9aa5e5c
ErikV121 <ErikVasquez121@gmail.com>
2024-07-14 21:29:37 -0400

    Implemented Get requests for id, isbn and title

--- src/main/java/com/Library_Management_Rest_API/controller/BookController.java
+++ src/main/java/com/Library_Management_Rest_API/controller/BookController.java
@@ -1,18 +1,14 @@
 package com.Library_Management_Rest_API.controller;
 
-import com.Library_Management_Rest_API.entity.Book;
 import com.Library_Management_Rest_API.payload.BookDto;
-import com.Library_Management_Rest_API.service.BookService;
 import com.Library_Management_Rest_API.service.impl.BookServiceImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/lib")
+@RequestMapping("/api/v1/library/")
 public class BookController {
 
     private final BookServiceImpl bookServiceimpl;
@@ -20,10 +16,27 @@ public class BookController {
     public BookController(BookServiceImpl bookServiceimpl) {
         this.bookServiceimpl = bookServiceimpl;
     }
-
     @PostMapping
     public ResponseEntity<BookDto> addBook(@RequestBody BookDto bookdto) {
         return new ResponseEntity<>(bookServiceimpl.createBook(bookdto), HttpStatus.CREATED);
     }
-    //TODO: Implement request methods
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<BookDto> getBookById(@PathVariable(name = "id") long id) {
+        System.out.println("id");
+        return new ResponseEntity<>(bookServiceimpl.getBookById(id), HttpStatus.OK);
+    }
+
+    @GetMapping("/isbn/{isbn}")
+    public ResponseEntity<BookDto> getBookByIsbn(@PathVariable String isbn) {
+        System.out.println("isbn");
+        return new ResponseEntity<>(bookServiceimpl.getBookByIsbn(isbn), HttpStatus.OK);
+    }
+
+    @GetMapping("/title/{title}")
+    public ResponseEntity<List<BookDto>> getListOfBooks(@PathVariable String title) {
+        return new ResponseEntity<>(bookServiceimpl.getBooksByTitle(title), HttpStatus.OK);
+
+    }
+
 }
